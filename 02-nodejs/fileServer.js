cc7da85f8a521a296559c45be335cf05be065b9a
MrDeepThought <singharshpreet55@gmail.com>
2023-06-21 16:00:05 +0530

    Updated second path/route since it was wrongly written and added middleware to handle invalid routes in the app

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -20,6 +20,52 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 3000;
 
+// global constants and variables
+const DIRPATH = path.resolve("files");
 
+// app middlewares
+
+// app handler functions
+function getFiles(req,res){
+  fs.readdir(DIRPATH, (err,files) => {
+    if (err){
+      res.status(500).send("Internal Sever Error!: \n" + err);
+    }
+    res.status(200).json(files);
+  })
+  
+}
+
+function getFile(req,res){
+  let filename = req.params.filename;
+  
+  fs.readdir(DIRPATH, (err,files) => {
+    let ind = files.indexOf(filename);
+    if (ind !== -1){
+      fs.readFile(path.join(DIRPATH,filename),'utf-8', (err,contents) => {
+        if (err){
+          console.log(err);
+          res.send(500).send("Internal Server Error. Can't open the file!");
+        }
+        res.status(200).send(contents);
+      });
+    }
+    else res.status(404).send("File not found");
+  });
+}
+
+// app routes
+app.get("/files", getFiles);
+app.get("/file/:filename", getFile);
+
+// checking if the HTTP server is working on the given port or not
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// });
+app.use((req, res, next) => {
+  res.status(404).send("Route not found")
+})
+// This is how you can handle routes that are not present in the app : https://expressjs.com/en/starter/faq.html#:~:text=How%20do%20I%20handle%20404,middleware%20will%20not%20capture%20them.
 module.exports = app;
